74e24e31a4534fffc3bc3528447a19700bc2e9d5
cbcaetanoborges@gmail.com <cbcaetanoborges@gmail.com>
2024-10-12 02:10:32 +0100

    andando

--- Classes/Cadastrar.php
+++ Classes/Cadastrar.php
@@ -2,80 +2,82 @@
 
 namespace Classes;
 
-use Ferramentas\AX;
 
 class Cadastrar
 {
-    protected $db;       
-    protected $array;
     protected $funcoes;
-    protected $tabela;
+    protected $conexao;
 
-    public function __construct($db, $funcoes, $tabela, $array) 
+    public function __construct($conexao,$funcoes) 
     {
-        /*
-        $arrayStructure = [
-            'insert'=>[],
-            'where'=>["campo" => $campo],
-            'count'=>''
-        ]
-        */
-
-        $this->db = $db;
-        $this->array = $array;
+        $this->conexao = $conexao;
         $this->funcoes = $funcoes;
-        $this->tabela = $tabela;
     }
 
     protected function _check()
     {
-        $count = $this->array['count'];
-        $where = $this->array['where'];
         
-        $res = $this->db->count($count)
-        ->from($this->tabela)
-        ->where([$where])
-        ->pegaResultado();
-        if($res[$count] > 0){
-            return true;
-        }else{
-            return false;
-        }
         
     }
 
-    public function verifica(){
-        $user = $this->_check();
-        if ($user) {
+    public function existeBi($bi){
+        $query=$this->conexao->prepare("SELECT * FROM particular WHERE bi = :bi");
+        $query->bindValue(':bi', $bi);
+        $query->execute();
+        if($query->rowCount() > 0){
             return true;
         }else{
             return false;
         }
     }
+    public function existeNif($nif){
+        $query=$this->conexao->prepare("SELECT * FROM empresa WHERE nif = :nif");
+        $query->bindValue(':nif', $nif);
+        $query->execute();
+        if($query->rowCount() > 0){
+            return true;
+        }else{
+            return false;        
+        }
+    }
+    public function existeTelefone($telefone){
+        $query=$this->conexao->prepare("SELECT * FROM contacto WHERE telefone = :telefone");
+        $query->bindValue(':telefone', $telefone);
+        $query->execute();
+        if($query->rowCount() > 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function enviaCodigo($telefone){
+        $this->funcoes::setRemetente('FETA-FACIL');
+        $codigo = $this->funcoes::seisDigitos();
+        $mensagem = "Caro cidadão, o seu código de confirmação para o cadastro é: $codigo";
+        $this->funcoes::enviaSMS($telefone, $mensagem);
 
-    public function cadastrar()
+        $query=$this->conexao->prepare("INSERT INTO confirmar (cliente_identificador, acao, codigo_enviado, quando, confirmou) VALUES (:cliente, :acao, :codigo, :quando, :corfirmou)");
+        $query->bindValue(':cliente', $telefone);
+        $query->bindValue(':acao', "cadastro");
+        $query->bindValue(':codigo', $codigo);
+        $query->bindValue(':quando', $this->funcoes::quando(time()));
+        $query->bindValue(':corfirmou', 0);
+        $query->execute();
+
+        return true;
+    }
+    public function verificaCodigo($telefone, $codigo){
+        
+    }
+
+    public function cadastrarParticular()
     {
-        $insert = $this->array['insert'];
-        
-        $user = $this->_check();
-        if ($user) {
-            return false;
-        }else{
-            $res = $this->novoUser($insert);
-            return $res;
-        }
+       
         
     }
-
-    public function novoUser($insert)
+    public function cadastrarEmpresa()
     {
-        $res = $this->db->insert($this->tabela,$insert)
-        ->executaQuery();
-
-        if ($res) {
-            return true;
-        }
-        return false;
+       
+        
     }
-
 }
--- Classes/testes.php
+++ Classes/testes.php
@@ -1,45 +1,15 @@
 <?php
 
-use Ferramentas\AX;
 use Ferramentas\Funcoes;
 use Classes\Cadastrar;
-use Classes\dbWrapper;
 
 require '../vendor/autoload.php';
 
-$dados = [
-    'nome'=>'Um nome',
-    'telefone'=>'943124678',
-    'palavra_passe'=>'yuytewtruweuqu'
-];
-
-$extra = ['extra'=>Funcoes::seisDigitos()];
-$identificador = ['identificador'=>Funcoes::chaveDB()];
-$insert = array_merge($identificador,$dados,$extra);
-
-
-//DONE
-$where = ['telefone'=>$dados['telefone']];
-foreach($where as $key => $value){
-    $where = $key.'='.AX::attr($value);
-}
-$count = 'telefone';
-
-foreach($insert as $key => $value){
-    $insert[$key] = AX::attr($value);
-}
-
-
 
 $funcoes = new Funcoes;
-$db = new dbWrapper($funcoes::conexao());
-$tabela = AX::tb('administrador');
-$arrayStructure['insert'] = $insert;
-$arrayStructure['where'] = $where;
-$arrayStructure['count'] = $count;
-
-$cadastrar = new Cadastrar($db,$funcoes,$tabela,$arrayStructure);
-$res = $cadastrar->cadastrar();
+$conexao = $funcoes->conexao();
+$cadastrar = new Cadastrar($conexao,$funcoes);
+$res = $cadastrar->enviaCodigo("921797626");
 
 var_dump($res);
 
